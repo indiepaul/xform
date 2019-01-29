e803bbe6d8557a3a6d47f6a9cb30d40e2d6f0572
Paul Mtali <paul.mtali@techie.com>
2019-01-29 09:14:31 +0200

    chenged default values to dynamic

--- lib/src/xfocusnode.dart
+++ lib/src/xfocusnode.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class XFocusNode {
   bool autoFocus = false;
   FocusNode focus;
-  String defaultValue;
+  dynamic defaultValue;
   XFocusNode({this.focus, this.autoFocus = false, this.defaultValue});
 }
